e3d69177257d6201fb72d16b75f59c5ae12c9faa
NgocPT <ngocpt@bkav.com>
2022-07-15 00:56:28 +0700

    Add save bitmap file.

---
+++ src/Image.hpp
@@ -0,0 +1,19 @@
+#ifndef __IMAGE_HPP__
+#define __IMAGE_HPP__
+
+#include <string>
+
+enum TEMPLATE {BLACK, WHITE};
+
+class Image {
+public:
+    Image(unsigned int, unsigned int, TEMPLATE = BLACK);
+    ~Image();
+    void save(std::string);
+private:
+    unsigned int width = 0;
+    unsigned int height = 0;
+    unsigned char *data = nullptr;
+};
+
+#endif
